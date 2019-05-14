4b062812f6d81e4a6b224ea27c60e17152a365f9
Abiral Rai <abiralrai182@gmail.com>
2019-05-14 18:22:10 +1000

    heroku bug

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cors());
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+// }
 
 // app.use(express.static('public'));
 app.use(bodyParser.json());
