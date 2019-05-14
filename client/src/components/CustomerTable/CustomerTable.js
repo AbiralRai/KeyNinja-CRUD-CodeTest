b7f05068de61fcff5e5bd29131d142fab04b5c88
Abiral Rai <abiralrai182@gmail.com>
2019-05-14 18:24:51 +1000

    heroku bug

--- client/src/components/CustomerTable/CustomerTable.js
+++ client/src/components/CustomerTable/CustomerTable.js
@@ -8,7 +8,7 @@ class CustomerTable extends Component {
   render() {
     let customers = this.props.customers;
 
-    customers = customers.map((customer, index) => (
+    customers = customers.map((customer, index) => 
       <Table.Row key={customer._id}>
         <Table.Cell>{index + 1}</Table.Cell>
         <Table.Cell>{customer.firstname}</Table.Cell>
@@ -34,7 +34,7 @@ class CustomerTable extends Component {
           />
         </Table.Cell>
       </Table.Row>
-    ));
+    );
 
 
     return (
