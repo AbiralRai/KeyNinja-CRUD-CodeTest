ec5caa3b9622437c370103448e79d2991d0e21b2
Abiral Rai <abiralrai182@gmail.com>
2019-05-14 16:13:10 +1000

    heroku

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 //Server static asset if in production
 if (process.send.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("public"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
