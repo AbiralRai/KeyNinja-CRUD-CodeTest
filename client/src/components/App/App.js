6b0ac2451c74bb210a758d621bbdca8be6683512
Abiral Rai <abiralrai182@gmail.com>
2019-05-14 20:07:34 +1000

    check server

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -13,8 +13,8 @@ class App extends Component {
   };
 
   // server = "http://localhost:5000";
-  server = "https://agile-inlet-32982.herokuapp.com";
-  // server = process.env.REACT_APP_API_URL || "https://localhost:5000";
+  // server = "https://agile-inlet-32982.herokuapp.com";
+  server = process.env.REACT_APP_API_URL || "https://localhost:5000";
 
   componentDidMount() {
     console.log("server::", this.server);
