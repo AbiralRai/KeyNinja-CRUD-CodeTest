908e80dffe354158f83c770b8425d08ced578c80
Abiral Rai <abiralrai182@gmail.com>
2019-05-14 15:21:58 +1000

    Added mongodb atlas

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require("cors");
+const path = require('path');
 
 //set up express app
 const app = express();
 // Unable to connect to mongodb atlas
 // const db = "mongodb+srv://abiral:abc@cluster0-rmrxf.mongodb.net/test?retryWrites=true";
+const db =
+  "mongodb://abiral:abc@cluster0-shard-00-00-rmrxf.mongodb.net:27017,cluster0-shard-00-01-rmrxf.mongodb.net:27017,cluster0-shard-00-02-rmrxf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 const dbLocal = "mongodb://localhost/CustomerDB";
 
 //Fix mongodb deprecation warnings
@@ -16,15 +19,27 @@ mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise; // use ES6 promise instead
 
 //Connect to mongodb
-mongoose.connect(dbLocal, {
+mongoose.connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true
 } )
 .then(() => { console.log('Database is connected') },
     err => { console.log('Can not connect to the database' + err) }
 );
-  app.use(cors());
-app.use(express.static('public'));
+
+app.use(cors());
+
+//Server static asset if in production
+if (process.send.NODE === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// app.use(express.static('public'));
 app.use(bodyParser.json());
 
 //initialize routess
