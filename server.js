f06874c1485d55c128de5ecfcad38ffef3b19103
Abiral Rai <abiralrai182@gmail.com>
2020-12-08 10:28:21 +1100

    Removed MONGODB API secret

--- server.js
+++ server.js
@@ -1,58 +1,51 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const path = require('path');
 
 //set up express app
 const app = express();
-// Unable to connect to mongodb atlas
-// const db = "mongodb+srv://abiral:abc@cluster0-rmrxf.mongodb.net/test?retryWrites=true";
-const db =
-  "mongodb://abiral:abc@cluster0-shard-00-00-rmrxf.mongodb.net:27017,cluster0-shard-00-01-rmrxf.mongodb.net:27017,cluster0-shard-00-02-rmrxf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-const dbLocal = "mongodb://localhost/CustomerDB";
 
 //Fix mongodb deprecation warnings
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useFindAndModify", false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise; // use ES6 promise instead
 
-const db =
-  "mongodb://abiral:abc@cluster0-shard-00-00-rmrxf.mongodb.net:27017,cluster0-shard-00-01-rmrxf.mongodb.net:27017,cluster0-shard-00-02-rmrxf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
-const dbLocal = "mongodb://localhost/CustomerDB";
-
+const db = process.env.MONGO_DB;
 //Connect to mongodb
 mongoose
   .connect(db, {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(
     () => {
-      console.log("Database is connected");
+      console.log('Database is connected');
     },
-    err => {
-      console.log("Can not connect to the database" + err);
+    (err) => {
+      console.log('Can not connect to the database' + err);
     }
   );
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 //initialize routess
-app.use("/api/customers/", require("./routes/customer.route"));
+app.use('/api/customers/', require('./routes/customer.route'));
 
 //error handling
 app.use((err, req, res, next) => {
   res.status(422).send({ error: err.message });
 });
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //Listening for request
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
